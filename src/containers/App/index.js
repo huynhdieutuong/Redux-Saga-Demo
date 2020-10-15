587e42f123a94ec2d5917ccb863d836651f572e5
Huynh Dieu Tuong <huynhdieutuong@gmail.com>
2020-10-15 20:10:10 +0700

    Implement Toastify

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -5,6 +5,8 @@ import Taskboard from '../Taskboard';
 import theme from '../../commons/Theme';
 import { Provider } from 'react-redux';
 import configureStore from '../../redux/configureStore';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const store = configureStore();
 
@@ -13,6 +15,7 @@ class App extends Component {
     return (
       <Provider store={store}>
         <ThemeProvider theme={theme}>
+          <ToastContainer />
           <Taskboard />
         </ThemeProvider>
       </Provider>
--- src/sagas/index.js
+++ src/sagas/index.js
@@ -2,6 +2,7 @@ import { call, put, takeLatest } from 'redux-saga/effects';
 import { getList } from '../apis/task';
 import { fetchListTaskSuccess, fetchListTaskFailed } from '../actions/task';
 import * as taskConstants from '../contants/task';
+import { toast } from 'react-toastify';
 
 function* watchFetchListTaskAction() {
   try {
@@ -9,6 +10,7 @@ function* watchFetchListTaskAction() {
     yield put(fetchListTaskSuccess(res.data));
   } catch (error) {
     yield put(fetchListTaskFailed(error));
+    toast.error(error.message);
   }
 }
 
